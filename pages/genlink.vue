dd0a42685a04ff59be54be8f4dcfc164c3156f6a
sasithornmai <89633039+sasithornmai@users.noreply.github.com>
2022-07-22 18:14:52 +0700

    add gen qrcode

---
+++ pages/genlink.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- {{ window.location }} -->
+    <button>download</button>
+    <div v-for="(quiz, i) in $store.state.quiz" :key="i">
+      <button @click="$router.push(`/quiz?id=${quiz.id}`)">
+        {{ quiz.id }}
+      </button>
+      <img
+        :src="`https://api.qrserver.com/v1/create-qr-code/?size=720x720&data=${url}?id=${quiz.id}`"
+        width="200"
+      />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LinkGen',
+  data: () => ({
+    url: 'http://localhost:3000/prepro-quiz',
+  }),
+}
+</script>
