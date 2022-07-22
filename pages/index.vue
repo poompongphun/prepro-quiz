50d0749119873ec51b3f4adb04c0a4801c501a26
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2022-07-22 20:29:50 +0700

    add: v-for

--- pages/index.vue
+++ pages/index.vue
@@ -11,106 +11,25 @@
       <div class="mb-4">
         <p class="text-2xl">Corrected answers</p>
         <div class="w-full flex flex-wrap justify-start items-center gap-3">
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-green-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-green-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-green-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-green-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-green-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-green-500">1</div>
+          <div v-for="(item, index) in correct_list" :key="index" class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-green-500">
+          {{index + 1}}
+          </div>
         </div>
       </div>
       <div class="mb-4">
         <p class="text-2xl">Incorrect answers</p>
         <div class="w-full flex flex-wrap justify-start items-center gap-3">
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">1</div>
+          <div v-for="(item, index) in incorrect_list" :key="index" class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-red-500">
+            {{index + 1}}
+          </div>
         </div>
       </div>
       <div class="mb-4">
         <p class="text-2xl">Answers left</p>
         <div class="w-full flex flex-wrap justify-start items-center gap-3">
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-gray-300">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-gray-300">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-gray-300">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-gray-300">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-gray-300">1</div>
-          <div class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-gray-300">1</div>
+          <div v-for="(item, index) in list_left" :key="index" class="w-10 h-10 flex items-center justify-center text-white rounded-xl bg-gray-300">
+            {{index + 1}}
+          </div>
         </div>
       </div>
     </div>
@@ -126,6 +45,9 @@ export default {
       return {
         title: "BlackSlash",
         score: 18,
+        correct_list: ["test"],
+        incorrect_list: ["test"],
+        list_left: ["test", "test"],
       }
     },
     methods: {
